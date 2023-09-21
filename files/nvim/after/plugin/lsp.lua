6c93a33ad7757991320ce8dba70c4afd5c326230
zyr4c31 <zyr4c31@gmail.com>
2023-09-21 10:54:48 +0800

    working symlink, new configs

--- files/nvim/after/plugin/lsp.lua
+++ files/nvim/after/plugin/lsp.lua
@@ -1,4 +1,12 @@
-local lsp = require("lsp-zero")
+local lsp_zero = require('lsp-zero')
 
-lsp.preset("recommended")
-lsp.setup()
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+---
+-- Replace these language servers
+-- with the ones you have installed in your system
+---
